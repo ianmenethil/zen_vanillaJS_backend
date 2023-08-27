53fc6e02cdecdf1081790326fb83ae43239b8c22
Ian Menethil <62359909+ianmenethil@users.noreply.github.com>
2023-08-28 01:16:50 +1000

    dev

--- main.js
+++ main.js
@@ -1,50 +1,72 @@
+// Code block is executed after the DOM has been fully loaded
 document.addEventListener("DOMContentLoaded", () => {
+  // Getting references to buttons on the page
   const nextButton = document.getElementById("next-button");
   const backButton = document.getElementById("back-button");
   const payNowButton = document.getElementById("pay-now-button");
+  // Attaching event listeners to the buttons
   nextButton.addEventListener("click", handleNextButtonClick);
   backButton.addEventListener("click", handleBackButtonClick);
   payNowButton.addEventListener("click", handlePayNowButtonClick);
-
+  // Getting references to all product checkboxes on the page
   const productCheckboxes = document.querySelectorAll('.product-showcase input[type="checkbox"]');
+  // Attaching a change event listener to each checkbox to update the total amount whenever a checkbox is checked or unchecked
   productCheckboxes.forEach((checkbox) => {
     checkbox.addEventListener("change", updateTotalAmount);
   });
+  // Updating the total amount initially when the page loads
   updateTotalAmount();
 });
 
-function updateTotalAmount() {
+// Function to calculate the total amount based on selected products
+function calculateTotalAmount() {
   const selectedProducts = document.querySelectorAll('.product-showcase input[type="checkbox"]:checked');
   let totalAmount = 0;
   selectedProducts.forEach((product) => {
     totalAmount += Number(product.value);
   });
+  return totalAmount; // Returning the calculated total amount
+}
 
+// Function to update the total amount based on selected products
+function updateTotalAmount() {
+  const totalAmount = calculateTotalAmount();
   document.getElementById("totalValue").textContent = "AUD $" + totalAmount;
   document.getElementById("registrationTotal").textContent = "AUD $" + totalAmount;
   document.getElementById("amountPayable").textContent = "AUD $" + totalAmount;
 }
 
+// Function to handle the behavior when the "Next" button is clicked
 function handleNextButtonClick() {
   const form = document.getElementById("registration-form");
-  const selectedProducts = document.querySelectorAll('.product-showcase input[type="checkbox"]:checked');
-  if (form.checkValidity() && selectedProducts.length > 0) {
-    form.style.display = "none";
-    document.getElementById("confirmation-section").style.display = "block";
-  } else if (selectedProducts.length === 0) {
+  const confirmationSection = document.getElementById("confirmation-section");
+  const productsSection = document.querySelector(".product-showcase");
+  // Checking if the form is valid and at least one product is selected
+  if (form.checkValidity() && calculateTotalAmount() > 0) {
+    // If so, hiding the form and showing the confirmation section
+    form.classList.add("hidden");
+    confirmationSection.classList.remove("hidden");
+    productsSection.classList.remove("error");
+  } else if (calculateTotalAmount() === 0) {
+    // If no product is selected, showing an error message
+    productsSection.classList.add("error");
     alert("Please select at least one product from the list.");
   } else {
+    // If the form is not valid, reporting validity (will show browser's default validation messages)
     form.reportValidity();
   }
 }
 
+// Handle the behavior when the "Back" button is clicked.
 function handleBackButtonClick() {
-  document.getElementById("confirmation-section").style.display = "none";
-  document.getElementById("registration-form").style.display = "block";
+  // Hiding the confirmation section and showing the form
+  document.getElementById("confirmation-section").classList.add("hidden");
+  document.getElementById("registration-form").classList.remove("hidden");
 }
 
+// Function to handle the behavior when the "Pay Now" button is clicked
 async function handlePayNowButtonClick() {
-  console.log("Pay Now button clicked");
+  // Creating a formData object with data from the form inputs
   const formData = {
     title: document.getElementById("title").value,
     mobile: document.getElementById("mobile").value,
@@ -58,21 +80,23 @@ async function handlePayNowButtonClick() {
   selectedProducts.forEach((product) => {
     totalAmount += Number(product.value);
   });
-  const paymentData = { ...formData, wholeNumberAmount: totalAmount };
-  console.log("Initiating payment with data:", paymentData);
+  // Combining the formData and the calculated total amount into a paymentData object
+  const paymentData = { ...formData, wholeNumberAmount: calculateTotalAmount() };
   try {
+    // Sending a POST request to the "/initiate-payment" endpoint with the paymentData as JSON
     let response = await fetch("/initiate-payment", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(paymentData),
     });
+    // If the request was successful, redirecting to the URL received in the response
     if (response.ok) {
       let jsonResponse = await response.json();
       window.location.href = jsonResponse.redirectUrl;
     } else {
-      console.error("Server responded with status:", response.status);
+      alert("There was an issue initiating the payment. Please try again later.");
     }
   } catch (error) {
-    console.error("Error initiating payment:", error);
+    alert("Error initiating payment. Please check your connection and try again.");
   }
 }
--- server.js
+++ server.js
@@ -1,22 +1,33 @@
+// Importing necessary modules
 const express = require("express");
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const CryptoJS = require("crypto-js");
 const cors = require("cors");
+
+// Defining allowed origins based on NODE_ENV variable
 const allowedOrigins = process.env.NODE_ENV === "production" ? ["https://eventsairdemodev.azurewebsites.net"] : ["*"];
+
+// Creating an instance of the Express application
 const app = express();
 const PORT = process.env.PORT || 3000;
-const axios = require("axios");
-
+// const axios = require("axios");
 require("dotenv").config();
 console.log("Current NODE_ENV:", process.env.NODE_ENV);
-// Logging middleware
+
+// Middleware for logging incoming requests
 app.use((req, res, next) => {
   console.log("Incoming request:", req.method, req.path);
   next();
 });
+
+// Parsing JSON data in the request body
 app.use(bodyParser.json());
+
+// Allowing cross-origin requests from defined origins
 app.use(cors({ origin: allowedOrigins }));
+
+// Adding security headers using Helmet middleware
 app.use(helmet());
 
 // Change to this after testing //
@@ -31,6 +42,7 @@ app.use(helmet());
 //   }
 // }));
 
+// Custom CSP configuration
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -45,78 +57,106 @@ app.use(
     },
   })
 );
+
+// Serving static files from the current directory
 app.use(express.static(__dirname));
 
+// Route for handling payment initiation
 app.post("/initiate-payment", async (req, res) => {
   console.log("Received payment initiation request:", req.body);
+  // Extracting data from the request body
   const formData = req.body;
+  const wholeNumberAmountForFingerprint = formData.wholeNumberAmount * 100;
+
+  // Fetching API key, username, and password from environment variables
   const apiKey = process.env.API_KEY;
   const un = process.env.UN;
   const pw = process.env.PW;
+
+  // Setting payment mode and generating a unique ID for the payment
   const payMode = "0";
   const mUPID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  const strFingerprint = CryptoJS.SHA1(apiKey + "|" + un + "|" + pw + "|" + payMode + "|" + formData.wholeNumberAmount + "|" + mUPID).toString();
-  const paymentAmount = (formData.wholeNumberAmount / 100).toFixed(2);
+
+  // Generating fingerprint using CryptoJS library
+  const strFingerprint = CryptoJS.SHA1(apiKey + "|" + un + "|" + pw + "|" + payMode + "|" + wholeNumberAmountForFingerprint + "|" + mUPID).toString();
+
+  // Formatting payment amount and constructing payment data object
+  const paymentAmount = formData.wholeNumberAmount.toFixed(2);
   const paymentData = {
     IsJsPlugin: "True",
     Mode: payMode,
-    Token: "",
-    AdditionalReference: "",
-    AustralianBusinessNumber: "",
-    CallbackUrl: "",
-    CompanyName: "",
+    // Token: "",
+    // AdditionalReference: "",
+    // AustralianBusinessNumber: "",
+    // CallbackUrl: "",
+    // CompanyName: "",
     ContactNumber: formData.mobile,
     CustomerEmail: formData.email,
-    CustomerId: "",
-    CustomerName: `${formData.firstName}+${formData.lastName}`,
+    // CustomerId: "",
+    CustomerName: `${formData.firstName} ${formData.lastName}`,
     CustomerReference: formData.companyName,
-    HideHeader: "true",
-    HideMerchantLogo: "",
+    HideHeader: "True",
+    HideMerchantLogo: "False",
     PaymentAmount: paymentAmount,
     RedirectUrl: "https://eventsairdemodev.azurewebsites.net/response.html",
-    ReturnUrl: "",
+    // ReturnUrl: "",
     MerchantUniquePaymentId: mUPID,
-    SendConfirmationEmailToMerchant: "False",
+    SendConfirmationEmailToMerchant: "True",
     __ApiKey: apiKey,
     __Fingerprint: strFingerprint,
     HideTermsAndConditions: "False",
-    ShowFeeOnTokenising: "False",
-    ShowFailedPaymentFeeOnTokenising: "False",
-    AllowBankAcOneOffPayment: "False",
-    AllowPayIdOneOffPayment: "False",
-    AllowApplePayOneOffPayment: "False",
-    OverrideFeePayer: "0",
-    UserMode: "0",
-    MinHeight: "0",
+    // ShowFeeOnTokenising: "False",
+    // ShowFailedPaymentFeeOnTokenising: "False",
+    // AllowBankAcOneOffPayment: "False",
+    // AllowPayIdOneOffPayment: "False",
+    // AllowApplePayOneOffPayment: "False",
+    // OverrideFeePayer: "0",
+    // UserMode: "0",
+    // MinHeight: "0",
   };
   try {
     const baseUrl = "https://payuat.travelpay.com.au/Online/v3/Authorise?";
     const queryString = new URLSearchParams(paymentData).toString();
+    console.log("Redirect URL:", `${baseUrl}${queryString}`);
     const redirectUrl = `${baseUrl}${queryString}`;
 
+    // Sending the redirect URL in the response
     res.json({ redirectUrl: redirectUrl });
   } catch (error) {
+    // Handling error if payment initiation fails
     console.error("Error initiating payment:", error);
     res.status(500).json({ error: "Error initiating payment." });
   }
 });
 
-app.post("/generate-fingerprint", (req, res) => {
-  const apiKey = process.env.API_KEY;
-  const un = process.env.UN;
-  const pw = process.env.PW;
-  const { payMode, wholeNumberAmount, mUPID } = req.body;
-  console.log("req.body", req.body);
-  const strFingerprint = CryptoJS.SHA1(apiKey + "|" + un + "|" + pw + "|" + payMode + "|" + wholeNumberAmount + "|" + mUPID).toString();
-  console.log("strFingerprint", strFingerprint);
-  res.json({ fingerprint: strFingerprint });
-});
+// // Route for generating fingerprint
+// app.post("/generate-fingerprint", (req, res) => {
+//   // Fetching API key, username, and password from environment variables
+//   const apiKey = process.env.API_KEY;
+//   const un = process.env.UN;
+//   const pw = process.env.PW;
 
-app.get("/generate-mupid", (req, res) => {
-  const mUPID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  console.log("mUPID", mUPID);
-  res.json({ mUPID });
-});
+//   // Extracting data from the request body
+//   const wholeNumberAmountForFingerprint = req.body.wholeNumberAmount * 100; // Convert to whole number for fingerprint generation
+//   const { payMode, mUPID } = req.body;
+//   console.log("req.body", req.body);
+
+//   // Generating fingerprint using CryptoJS library
+//   const strFingerprint = CryptoJS.SHA1(apiKey + "|" + un + "|" + pw + "|" + payMode + "|" + wholeNumberAmountForFingerprint + "|" + mUPID).toString();
+//   console.log("strFingerprint", strFingerprint);
+
+//   // Sending the generated fingerprint in the response
+//   res.json({ fingerprint: strFingerprint });
+// });
+
+// // Route for generating a unique ID for payment
+// app.get("/generate-mupid", (req, res) => {
+//   // Generating a unique ID using Math.random() and converting it to a string
+//   const mUPID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+//   console.log("mUPID", mUPID);
+//   // Sending the generated unique ID in the response
+//   res.json({ mUPID });
+// });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
