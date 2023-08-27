ee796a0d3a19ce8209104e99456eca8efb87184d
Ian Menethil <62359909+ianmenethil@users.noreply.github.com>
2023-08-27 14:57:04 +1000

    master

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const helmet = require("helmet");
 const CryptoJS = require("crypto-js");
 const cors = require("cors");
 const allowedOrigins = process.env.NODE_ENV === "production" ? ["https://eventsairdemo.azurewebsites.net"] : ["*"];
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Logging middleware
 app.use((req, res, next) => {
   console.log("Incoming request:", req.method, req.path);
   next();
@@ -16,12 +16,12 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(cors({ origin: allowedOrigins }));
 app.use(helmet());
+app.use(express.static(__dirname));
 
 app.post("/generate-fingerprint", (req, res) => {
   const apiKey = process.env.API_KEY || "defaultApiKey";
   const un = process.env.UN || "defaultUn";
   const pw = process.env.PW || "defaultPw";
-
   const { payMode, wholeNumberAmount, mUPID } = req.body;
   console.log("req.body", req.body);
   const strFingerprint = CryptoJS.SHA1(apiKey + "|" + un + "|" + pw + "|" + payMode + "|" + wholeNumberAmount + "|" + mUPID).toString();
@@ -35,6 +35,12 @@ app.get("/generate-mupid", (req, res) => {
   res.json({ mUPID });
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
