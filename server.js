1b8566929dc45fbbf0213dd03d5ed807674e1067
Ian Menethil <62359909+ianmenethil@users.noreply.github.com>
2023-08-27 23:20:20 +1000

    dev

--- server.js
+++ server.js
@@ -7,19 +7,13 @@ const allowedOrigins = process.env.NODE_ENV === "production" ? ["https://eventsa
 const app = express();
 const PORT = process.env.PORT || 3000;
 const axios = require("axios");
-
+require("dotenv").config();
+console.log("Current NODE_ENV:", process.env.NODE_ENV);
 // Logging middleware
 app.use((req, res, next) => {
-  // console.log("Incoming request:", req.method, req.path);
+  console.log("Incoming request:", req.method, req.path);
   next();
 });
-
-console.log("Server starting with the following environment variables:");
-console.log("API_KEY:", process.env.API_KEY ? "SET" : "NOT SET");
-console.log("UN:", process.env.UN ? "SET" : "NOT SET");
-console.log("PW:", process.env.PW ? "SET" : "NOT SET");
-console.log("NODE_ENV:", process.env.NODE_ENV);
-
 app.use(bodyParser.json());
 app.use(cors({ origin: allowedOrigins }));
 app.use(helmet());
@@ -50,26 +44,56 @@ app.use(
     },
   })
 );
-
 app.use(express.static(__dirname));
-
 app.post("/initiate-payment", async (req, res) => {
+  console.log("Received request for /initiate-payment with data:", req.body);
   const formData = req.body;
-  const apiKey = process.env.API_KEY || "defaultApiKey";
-  const un = process.env.UN || "defaultUn";
-  const pw = process.env.PW || "defaultPw";
-  const strFingerprint = CryptoJS.SHA1(apiKey + "|" + un + "|" + pw + "|" + formData.payMode + "|" + formData.wholeNumberAmount + "|" + formData.mUPID).toString();
-
+  const apiKey = process.env.API_KEY;
+  const un = process.env.UN;
+  const pw = process.env.PW;
+  const payMode = "0";
+  const mUPID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  const strFingerprint = CryptoJS.SHA1(apiKey + "|" + un + "|" + pw + "|" + payMode + "|" + formData.wholeNumberAmount + "|" + mUPID).toString();
+  const paymentAmount = (formData.wholeNumberAmount / 100).toFixed(2);
   const paymentData = {
+    IsJsPlugin: "True",
+    Mode: payMode,
+    Token: "",
+    AdditionalReference: "",
+    AustralianBusinessNumber: "",
+    CallbackUrl: "",
+    CompanyName: "",
+    ContactNumber: formData.mobile,
+    CustomerEmail: formData.email,
+    CustomerId: "",
+    CustomerName: `${formData.firstName}+${formData.lastName}`,
+    CustomerReference: formData.companyName,
+    HideHeader: "true",
+    HideMerchantLogo: "",
+    PaymentAmount: paymentAmount,
+    RedirectUrl: "https://eventsairdemodev.azurewebsites.net/response.html",
+    ReturnUrl: "",
+    MerchantUniquePaymentId: mUPID,
+    SendConfirmationEmailToMerchant: "False",
     __ApiKey: apiKey,
     __Fingerprint: strFingerprint,
+    HideTermsAndConditions: "False",
+    ShowFeeOnTokenising: "False",
+    ShowFailedPaymentFeeOnTokenising: "False",
+    AllowBankAcOneOffPayment: "False",
+    AllowPayIdOneOffPayment: "False",
+    AllowApplePayOneOffPayment: "False",
+    OverrideFeePayer: "0",
+    UserMode: "0",
+    MinHeight: "0",
   };
-
   try {
+    console.log("Making a request to payment API:", "https://payuat.travelpay.com.au/Online/Payment?version=v3", paymentData);
     const paymentResponse = await axios.post("https://payuat.travelpay.com.au/Online/Payment?version=v3", paymentData);
-    const redirectUrl = paymentResponse.data.url;
-    res.json({ redirectUrl });
+    console.log("Response from payment API:", paymentResponse.data);
+    res.send(paymentResponse.data);
   } catch (error) {
+    console.error("Error initiating payment:", error);
     res.status(500).json({ error: "Error initiating payment." });
   }
 });
@@ -79,15 +103,15 @@ app.post("/generate-fingerprint", (req, res) => {
   const un = process.env.UN;
   const pw = process.env.PW;
   const { payMode, wholeNumberAmount, mUPID } = req.body;
-  // console.log("req.body", req.body);
+  console.log("req.body", req.body);
   const strFingerprint = CryptoJS.SHA1(apiKey + "|" + un + "|" + pw + "|" + payMode + "|" + wholeNumberAmount + "|" + mUPID).toString();
-  // console.log("strFingerprint", strFingerprint);
+  console.log("strFingerprint", strFingerprint);
   res.json({ fingerprint: strFingerprint });
 });
 
 app.get("/generate-mupid", (req, res) => {
   const mUPID = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  // console.log("mUPID", mUPID);
+  console.log("mUPID", mUPID);
   res.json({ mUPID });
 });
 
@@ -98,5 +122,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  // console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
