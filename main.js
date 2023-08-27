0675539063c7af672f152a4f7e084b2de8a1813e
Ian Menethil <62359909+ianmenethil@users.noreply.github.com>
2023-08-27 13:11:37 +1000

    master

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nextButton = document.getElementById("next-button");
   const backButton = document.getElementById("back-button");
-  // const payNowButton = document.getElementById('pay-now-button');
-
   nextButton.addEventListener("click", handleNextButtonClick);
   backButton.addEventListener("click", handleBackButtonClick);
-  // payNowButton.addEventListener('click', handlePayNowButtonClick);
 });
 
 function handleNextButtonClick() {
